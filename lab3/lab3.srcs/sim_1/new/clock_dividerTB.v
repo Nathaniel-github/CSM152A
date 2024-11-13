51d91dc42aa012653afbfa5d0ab62e511fcc6ed9
eayala720 <eayala720@gmail.com>
2024-11-12 16:07:42 -0800

    finished clock_divider.v module

--- lab3/lab3.srcs/sim_1/new/clock_dividerTB.v
+++ lab3/lab3.srcs/sim_1/new/clock_dividerTB.v
@@ -21,18 +21,40 @@
 
 
 module clock_dividerTB;
-
-    wire input_clk = 100_000_000;
-    wire output_clk;
+    // Inputs
+    reg master;     // master clock
+    reg reset;      // reset
+    
+    // Outputs
+    wire two;       // 2 Hz clock
+    wire one;       // 1 Hz clock
+    wire faster;    // 200 Hz clock
+    wire adj;       // ?? Hz clock
     
     clock_divider clk_test(
-        .master_clk(input_clk),
-        .two_clk(output_clk)
+        .clk(master),
+        .rst(reset),
+        .two_clk(two),
+        .one_clk(one),
+        .faster_clk(faster),
+        .adj_clk(adj)
     );
     
-    initial
-    begin
-    
+    // Initialize the master clock.
+    // Since the clock operates at 100 MHz, each clock cycle is 10 ns as T (period) = 1/f (frequency).
+    // [T = 1/f = 1/100 Mhz = 1/(100_000_000 cycle/sec)= 10 ns]
+    // (i.e. at 100 MHz, there are 100 million periods in one sec & each period is 10 ns)
+    initial begin
+        // set the master clock to 0
+        master = 0;
+        reset = 1;
+        #100;
+
+        reset = 0;
+        #100;
     end
     
+    // toggle master clock b/w 0 and 1 to simulate a continous clock signal
+    // delay the forever loop by 5 ns each iteration [as (10 ns)/2 = 5 ns per half cycle]
+    always #5 master = ~master;
 endmodule
--- lab3/lab3.srcs/sources_1/new/clock_divider.v
+++ lab3/lab3.srcs/sources_1/new/clock_divider.v
@@ -22,77 +22,95 @@
 // Clock Module that takes the 100 MHz master clock and outputs 4 clocks:
 // 2 Hz, 1 Hz, ?? Hz, and adjusment mode clock
 module clock_divider(
-    input wire master_clk,
+    input wire clk,
+    input wire rst,
     output wire two_clk,
     output wire one_clk,
     output wire faster_clk,
-    output wire adj_mode_clk
+    output wire adj_clk
     );
     
-    reg[25:0] two_counter;
-    reg[25:0] one_counter;
-    reg[17:0] faster_counter;
-    reg[24:0] adj_counter;
+    reg[25:0] two_counter = 0;
+    reg[25:0] one_counter = 0;
+    reg[25:0] faster_counter = 0;
+    reg[25:0] adj_counter = 0;
     
-    reg two_temp;
-    reg one_temp;
-    reg faster_temp;
-    reg adj_temp;
+    reg two_temp = 0;
+    reg one_temp = 0;
+    reg faster_temp = 0;
+    reg adj_temp = 0;
     
-    initial begin
-        two_counter = 0;
-        one_counter = 0;
-        faster_counter = 0;
-        adj_counter = 0;
-        
-        two_temp = 0;
-        one_temp = 0;
-        faster_temp = 0;
-        adj_temp = 0;
-    end
-    
-    always @(posedge master_clk)
+    always @ (posedge (clk) or posedge (rst))
     begin
-        // 100 MHz = 100,000,000 Hz; 50_000_000 is one cycle
-        if (two_counter == 25_000_000 - 1) begin
+        // 2 Hz clock
+        // To get 2 Hz (2 cps) from 100 MHz, divide the total cycles in 100 MHz by 2: 100,000,000/2 = 50,000,000.
+        // Every 50,000,000 cycles in a 100 MHz signal produces 1 cycle in a 2 Hz signal.
+        // Each cycle takes 0.5 seconds [T = 1/f = 1/2 Hz = 0.5 seconds]. 
+        // When two_counter reaches 25,000,000, toggle two_temp as we've reached half of one cycle (period).
+        // Since there are 100_000_000 cycles, there are 4 toggles in total.
+        if (rst == 1) begin
+            two_counter <= 0;
+            two_temp <= 0;
+        end
+        else if (two_counter == 25_000_000 - 1) begin
             two_counter <= 0;
             two_temp <= ~two_temp;
         end
         else begin
-            two_temp <= two_temp;
             two_counter <= two_counter + 1;
-        end 
-        
-        if (one_counter == 50_000_000 - 1) begin
+            two_temp <= two_temp;
+        end
+    end
+      
+    always @ (posedge (clk) or posedge (rst))
+    begin
+        if (rst == 1) begin
+            one_counter <= 0;
+            one_temp <= 0;
+        end
+        else if (one_counter == 50_000_000 - 1) begin
             one_counter <= 0;
             one_temp <= ~one_temp;
         end
         else begin
-            one_temp <= one_temp;
             one_counter <= one_counter + 1;
+            one_temp <= one_temp;
         end 
-        
-        if (faster_counter == 250_000 - 1) begin
+    end
+     
+    always @ (posedge (clk) or posedge (rst))
+    begin
+        if (rst == 1) begin
+            faster_counter <= 0;
+            faster_temp <= 0;
+        end
+        else if (faster_counter == 250_000 - 1) begin
             faster_counter <= 0;
             faster_temp <= ~faster_temp;
         end
         else begin
-            faster_temp <= faster_temp;
             faster_counter <= faster_counter + 1;
-        end 
-        
-        if (adj_counter == 25_000_000 - 1) begin
+            faster_temp <= faster_temp;
+        end
+    end
+     
+    always @ (posedge (clk) or posedge (rst))
+    begin
+        if (rst == 1) begin
+            adj_counter <= 0;
+            adj_temp <= 0;
+        end
+        else if (adj_counter == 25_000_000 - 1) begin
             adj_counter <= 0;
             adj_temp <= ~adj_temp;
         end
         else begin
-            adj_temp <= adj_temp;
             adj_counter <= adj_counter + 1;
-        end 
-        
+            adj_temp <= adj_temp;
+        end
     end
     
-    assign two_clk = two_temp;
+    assign two_clk = two_temp; 
     assign one_clk = one_temp;
     assign faster_clk = faster_temp;
     assign adj_clk = adj_temp;
